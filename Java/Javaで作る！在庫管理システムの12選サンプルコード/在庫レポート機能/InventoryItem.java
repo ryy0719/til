8d87e91a9e653036fd4ec9d7074a9460b0199a67
ryy0719 <124422821+ryy0719@users.noreply.github.com>
2024-08-18 16:42:18 +0900

    Create InventoryItem.java

---
+++ Java/Javaで作る！在庫管理システムの12選サンプルコード/在庫レポート機能/InventoryItem.java
@@ -0,0 +1,23 @@
+public class InventoryItem {
+    private String name;
+    private int quantity;
+    private int price;
+
+    public InventoryItem(String name, int quantity, int price) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
