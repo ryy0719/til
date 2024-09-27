95b1a049f3a2847d79ac8e29a3b64331a6acb22b
ryy0719 <124422821+ryy0719@users.noreply.github.com>
2024-09-27 10:34:24 +0900

    Create ChangeLocaleAction.java

---
+++ Java/Struts 2 database application/ProductManagementApplication/src/main/java/com/example/action/ChangeLocaleAction.java
@@ -0,0 +1,10 @@
+package com.example.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ChangeLocaleAction extends ActionSupport {
+
+    public String execute() {
+        return INPUT;
+    }
+}
